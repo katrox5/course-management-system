0e7b8b6d180506a6ca6e50aaec4f32af9be0a812
katrox <wildcityserver@qq.com>
2024-12-09 23:54:29 +0800

    feat: 调整容器中内容的表现效果

--- src/layouts/Navigation.vue
+++ src/layouts/Navigation.vue
@@ -14,40 +14,41 @@
 
     <div class="flex-grow-1 flex">
       <!-- 侧边导航栏 -->
-      <aside class="b-r-solid b-r-gray-3 b-r-[1px] w-200px">
+      <aside class="b-r-solid b-r-gray-3 b-r-[1px] w-240px">
         <el-menu :default-active="$route.fullPath" @select="handleSelect">
-          <!-- 用户视图 -->
-          <!-- TODO 识别用户权限 -->
-          <template v-if="true" v-for="item in itemsUser">
-            <el-menu-item :index="item.route">
-              <span>{{ item.label }}</span>
-            </el-menu-item>
-          </template>
-
-          <!-- 管理员视图 -->
-          <template v-else v-for="item in itemsAdmin">
+          <template v-for="item in items">
             <el-sub-menu v-if="item.children?.length" :index="item.route">
               <template #title>
-                <span>{{ item.label }}</span>
+                <span class="label">{{ item.label }}</span>
               </template>
               <template v-for="subItem in item.children">
                 <el-menu-item :index="item.route + subItem.route">
-                  <span>{{ subItem.label }}</span>
+                  <span class="label">{{ subItem.label }}</span>
                 </el-menu-item>
               </template>
             </el-sub-menu>
 
             <el-menu-item v-else :index="item.route">
-              <span>{{ item.label }}</span>
+              <span class="label">{{ item.label }}</span>
             </el-menu-item>
           </template>
         </el-menu>
       </aside>
 
       <!-- 内容 -->
-      <div class="flex-grow-1 bg-[#f4f3f3] overflow-y-auto relative">
-        <div class="absolute">
-          <slot />
+      <div class="flex-grow-1 bg-[#f4f3f3] relative overflow-y-auto">
+        <div class="w-full pt-4 px-4 absolute">
+          <el-breadcrumb class="ml-2" separator="/">
+            <template v-for="item in breadcrumb">
+              <el-breadcrumb-item v-if="item">
+                {{ item?.label }}
+              </el-breadcrumb-item>
+            </template>
+          </el-breadcrumb>
+
+          <div class="my-4 p-4 bg-white rounded-md">
+            <slot />
+          </div>
         </div>
       </div>
     </div>
@@ -61,6 +62,7 @@
     children?: Item[]
   }
 
+  const route = useRoute()
   const router = useRouter()
 
   function handleSelect(path: string) {
@@ -132,10 +134,26 @@
       route: '/project',
     },
   ]
+
+  const breadcrumb = computed(() => {
+    const firstItem = items.find((item) => route.fullPath.startsWith(item.route))
+    const secondItem = firstItem?.children?.find((item) => route.fullPath.endsWith(item.route))
+    return [firstItem, secondItem]
+  })
+
+  // TODO: 识别用户权限
+  const items = true ? itemsUser : itemsAdmin
 </script>
 
 <style>
   .el-menu .is-active {
     background-color: var(--el-menu-hover-bg-color);
+  }
+</style>
+
+<style scoped>
+  .label {
+    user-select: none;
+    font-size: 1.125rem;
   }
 </style>
--- src/pages/project/index.vue
+++ src/pages/project/index.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts"></script>
 
 <template>
-  <navigation />
+  <navigation>
+    <template v-for="i in 40">
+      <div>此处是插槽内容！</div>
+    </template>
+  </navigation>
 </template>
