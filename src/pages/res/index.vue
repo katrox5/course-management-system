21a6f7c1f6210da3d82541ff524d510044fd1810
YUxinxiao-484 <2031297821@qq.com>
2024-12-18 13:35:04 +0800

    Update index.vue

--- src/pages/res/index.vue
+++ src/pages/res/index.vue
@@ -1,3 +1,8 @@
-<script setup lang="ts"></script>
-
-<template></template>
+<template>
+    <el-card>
+      <h2>资源管理</h2>
+      <el-button type="primary" @click="$router.push('/res/manage')">管理资源</el-button>
+    </el-card>
+  </template>
+  
+  <script setup lang="ts"></script>
