c92b78f82cfef940e8cc83eb3a162ddb3c323246
YUxinxiao-484 <2031297821@qq.com>
2024-12-18 13:32:39 +0800

    Update index.vue

--- src/pages/info/index.vue
+++ src/pages/info/index.vue
@@ -1,3 +1,9 @@
-<script setup lang="ts"></script>
-
-<template></template>
+<template>
+    <el-card>
+      <h2>信息管理</h2>
+      <el-button type="primary" @click="$router.push('/info/list')">查看信息列表</el-button>
+      <el-button type="success" @click="$router.push('/info/post')">发布信息</el-button>
+    </el-card>
+  </template>
+  
+  <script setup lang="ts"></script>
