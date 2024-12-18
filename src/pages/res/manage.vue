a1fbe220570d6dee71975de7fa8852036202cd6e
YUxinxiao-484 <2031297821@qq.com>
2024-12-18 14:08:34 +0800

    Update manage.vue

--- src/pages/res/manage.vue
+++ src/pages/res/manage.vue
@@ -1,3 +1,43 @@
-<script setup lang="ts"></script>
-
-<template></template>
+<template>
+    <el-card>
+      <h2>资源管理</h2>
+      <el-upload
+        action="#"
+        :on-success="handleSuccess"
+        :on-preview="handlePreview"
+        list-type="text"
+      >
+        <el-button type="primary">上传资源</el-button>
+      </el-upload>
+  
+      <el-table :data="resources" style="width: 100%; margin-top: 20px">
+        <el-table-column prop="resource_name" label="资源名称" />
+        <el-table-column prop="file_path" label="文件路径" />
+        <el-table-column prop="uploaded_at" label="上传时间" width="200" />
+      </el-table>
+    </el-card>
+  </template>
+  
+  <script setup lang="ts">
+  import { ref, onMounted } from 'vue';
+  import axios from 'axios';
+  
+  const resources = ref([]);
+  
+  const fetchResources = async () => {
+    const response = await axios.get('/api/resources'); // 假设后端 API
+    resources.value = response.data;
+  };
+  
+  const handleSuccess = () => {
+    fetchResources();
+  };
+  
+  const handlePreview = (file: any) => {
+    window.open(file.url);
+  };
+  
+  onMounted(() => {
+    fetchResources();
+  });
+  </script>
