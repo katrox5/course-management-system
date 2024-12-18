2c097004225c483a2466888614490d876a2312bd
katrox <wildcityserver@qq.com>
2024-12-18 22:26:55 +0800

    revert: 回滚错误修改的文件

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
 import './assets/main.css'
 import 'virtual:uno.css'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
-import 'element-plus/theme-chalk/el-message.css'
+
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
@@ -11,7 +9,7 @@ import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
-app.use(ElementPlus)
+
 app.use(createPinia())
 app.use(router)
 
