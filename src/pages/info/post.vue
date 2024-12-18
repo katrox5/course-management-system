31e201be78f8e03b82341ac3d9811cbd97b401d5
YUxinxiao-484 <2031297821@qq.com>
2024-12-18 13:33:58 +0800

    Update post.vue

--- src/pages/info/post.vue
+++ src/pages/info/post.vue
@@ -1,3 +1,30 @@
-<script setup lang="ts"></script>
-
-<template></template>
+<template>
+    <el-card>
+      <h2>发布信息</h2>
+      <el-form :model="formData" label-width="80px">
+        <el-form-item label="标题">
+          <el-input v-model="formData.title" placeholder="输入标题" />
+        </el-form-item>
+        <el-form-item label="内容">
+          <el-input v-model="formData.content" type="textarea" :rows="4" placeholder="输入内容" />
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="submitForm">发布</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </template>
+  
+  <script setup lang="ts">
+  import { reactive } from 'vue';
+  
+  const formData = reactive({
+    title: '',
+    content: '',
+  });
+  
+  const submitForm = () => {
+    console.log('发布内容:', formData);
+    alert('发布成功！');
+  };
+  </script>
