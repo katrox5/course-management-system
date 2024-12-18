9a242c2ca4851920260a2d676b4b9a91f07b6c1e
YUxinxiao-484 <2031297821@qq.com>
2024-12-18 13:44:27 +0800

    Update post.vue

--- src/pages/info/post.vue
+++ src/pages/info/post.vue
@@ -1,12 +1,12 @@
 <template>
     <el-card>
-      <h2>发布信息</h2>
+      <h2>发布公告</h2>
       <el-form :model="formData" label-width="80px">
         <el-form-item label="标题">
-          <el-input v-model="formData.title" placeholder="输入标题" />
+          <el-input v-model="formData.title" />
         </el-form-item>
         <el-form-item label="内容">
-          <el-input v-model="formData.content" type="textarea" :rows="4" placeholder="输入内容" />
+          <el-input v-model="formData.content" type="textarea" rows="4" />
         </el-form-item>
         <el-form-item>
           <el-button type="primary" @click="submitForm">发布</el-button>
@@ -17,14 +17,15 @@
   
   <script setup lang="ts">
   import { reactive } from 'vue';
+  import axios from 'axios';
   
   const formData = reactive({
     title: '',
     content: '',
   });
   
-  const submitForm = () => {
-    console.log('发布内容:', formData);
-    alert('发布成功！');
+  const submitForm = async () => {
+    await axios.post('/api/announcements', formData);
+    alert('公告发布成功');
   };
   </script>
