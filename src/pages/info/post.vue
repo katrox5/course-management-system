c9ee77f7dab1f97a504225bf282679864ca4ee63
YUxinxiao-484 <2031297821@qq.com>
2024-12-18 14:50:41 +0800

    Update post.vue

--- src/pages/info/post.vue
+++ src/pages/info/post.vue
@@ -1,31 +1,3 @@
-<template>
-    <el-card>
-      <h2>发布公告</h2>
-      <el-form :model="formData" label-width="80px">
-        <el-form-item label="标题">
-          <el-input v-model="formData.title" />
-        </el-form-item>
-        <el-form-item label="内容">
-          <el-input v-model="formData.content" type="textarea" rows="4" />
-        </el-form-item>
-        <el-form-item>
-          <el-button type="primary" @click="submitForm">发布</el-button>
-        </el-form-item>
-      </el-form>
-    </el-card>
-  </template>
-  
-  <script setup lang="ts">
-  import { reactive } from 'vue';
-  import axios from 'axios';
-  
-  const formData = reactive({
-    title: '',
-    content: '',
-  });
-  
-  const submitForm = async () => {
-    await axios.post('/api/announcements', formData);
-    alert('公告发布成功');
-  };
-  </script>
+<script setup lang="ts"></script>
+
+<template></template>
