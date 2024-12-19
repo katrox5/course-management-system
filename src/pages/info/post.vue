87b3e1c6a2dc5b092492d812a1751648d38bdd47
katrox <wildcityserver@qq.com>
2024-12-19 22:17:27 +0800

    feat(/info/post): 调整布局

--- src/pages/info/post.vue
+++ src/pages/info/post.vue
@@ -1,61 +1,57 @@
 <template>
-  <!-- 背景矩形设置 -->
-  <div class="background-container">
-    <el-card>
-      <el-form label-width="100px">
-        <!-- 标题输入框 -->
-        <el-form-item label="标题">
-          <el-input v-model="form.title" placeholder="请输入标题" />
-        </el-form-item>
+  <div>
+    <h3 class="mb-4">信息发布</h3>
+    <el-form label-position="top">
+      <!-- 标题输入框 -->
+      <el-form-item label="标题">
+        <el-input v-model="form.title" placeholder="请输入标题" />
+      </el-form-item>
 
-        <!-- 有效期输入框 -->
-        <el-form-item label="有效期">
-          <el-date-picker
-            v-model="form.date"
-            type="daterange"
-            range-separator="-"
-            start-placeholder="开始日期"
-            end-placeholder="结束日期"
-          />
-        </el-form-item>
+      <!-- 有效期输入框 -->
+      <el-form-item label="有效期">
+        <el-date-picker
+          v-model="form.date"
+          type="daterange"
+          range-separator="-"
+          start-placeholder="开始日期"
+          end-placeholder="结束日期"
+        />
+      </el-form-item>
 
-        <!-- 固定大小的内容输入框 -->
-        <el-form-item label="内容">
-          <el-input
-            v-model="form.content"
-            type="textarea"
-            placeholder="请输入内容"
-            class="fixed-content-input"
-          />
-        </el-form-item>
+      <!-- 固定大小的内容输入框 -->
+      <el-form-item label="内容">
+        <el-input
+          v-model="form.content"
+          type="textarea"
+          resize="none"
+          placeholder="请输入内容"
+          class="fixed-content-input"
+        />
+      </el-form-item>
 
-        <!-- 图片上传 -->
-        <el-form-item label="图片">
-          <el-upload
-            v-model:file-list="form.imageList"
-            action="#"
-            list-type="picture-card"
-            :on-preview="handlePreview"
-            :on-remove="handleRemove"
-          >
-            <el-icon><plus /></el-icon>
-          </el-upload>
-        </el-form-item>
-      </el-form>
+      <!-- 图片上传 -->
+      <el-form-item label="图片">
+        <el-upload
+          v-model:file-list="form.imageList"
+          action="#"
+          list-type="picture-card"
+          :on-preview="handlePreview"
+          :on-remove="handleRemove"
+        >
+          <el-icon><el-icon-plus /></el-icon>
+        </el-upload>
+      </el-form-item>
+    </el-form>
 
-      <!-- 按钮容器 -->
-      <div class="button-container">
-        <el-button @click="cancelForm" class="cancel-button">取消</el-button>
-        <el-button type="primary" @click="submitForm">发布信息</el-button>
-      </div>
-    </el-card>
+    <!-- 按钮容器 -->
+    <div class="button-container">
+      <el-button @click="cancelForm" class="cancel-button">取消</el-button>
+      <el-button type="primary" @click="submitForm">发布信息</el-button>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-  import { ref } from 'vue'
-  import { Plus } from '@element-plus/icons-vue'
-
   const form = ref({
     title: '',
     date: [],
@@ -88,16 +84,6 @@
 </script>
 
 <style scoped>
-  /* 背景容器样式 */
-  .background-container {
-    width: 1000px;
-    height: 847px;
-    background-color: #f5f5f5;
-    margin: 0 auto;
-    padding: 20px;
-    box-sizing: border-box;
-  }
-
   /* 按钮容器靠右 */
   .button-container {
     display: flex;
